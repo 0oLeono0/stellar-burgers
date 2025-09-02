92bd8530a6c2f26baee9fb67b9a395774b591d87
unknown <lyonya_ivanovskij@mail.ru>
2025-09-02 13:11:38 +0300

    add: login

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,17 +1,27 @@
 import { FC, SyntheticEvent, useState } from 'react';
 import { LoginUI } from '@ui-pages';
+import { useDispatch, useSelector } from '../../services/store';
+import { useNavigate } from 'react-router-dom';
+import { errorSelector, loginUserThunk } from '../../services/slices/userSlice';
 
 export const Login: FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const dispatch = useDispatch();
+  const error = useSelector(errorSelector);
+  const navigate = useNavigate();
+
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
+    dispatch(loginUserThunk({ email, password })).then(() => {
+      navigate('/profile');
+    });
   };
 
   return (
     <LoginUI
-      errorText=''
+      errorText={error!}
       email={email}
       setEmail={setEmail}
       password={password}
